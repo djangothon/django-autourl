b124167b69609255247bc83c32bcc0678d6b5708
gururajbn <n.gururajbn@gmail.com>
2015-08-23 01:10:37 +0530

    added chart Visualization

--- ubuild/views.py
+++ ubuild/views.py
@@ -4,8 +4,19 @@ import json
 # Create your views here.
 def home(request):
 	log= urls.objects.all() 	
+	
 	return render(request,"ubuild/home.html",{
-		"log":log
+		"log":log,
+		"top_url1":log[0].httpRefer,
+		"top_url2":log[1].httpRefer,
+		"top_url3":log[2].httpRefer,
+		"top_url4":log[3].httpRefer,
+		"top_url5":log[4].httpRefer,	
+		"count1":log[0].count,
+		"count2":log[1].count,
+		"count3":log[2].count,
+		"count4":log[3].count,
+		"count5":log[4].count
 		})
 
 def details(request,url):
